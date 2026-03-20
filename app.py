cd3efd506727231b24bd21a449528c24321624fa
kilarisai <kpsai2004@gmail.com>
2026-03-20 12:46:47 +0530

    added port stability for deployment

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request
+import os
 import numpy as np
 import joblib
 from pytorch_tabnet.tab_model import TabNetClassifier
@@ -55,4 +56,5 @@ def predict():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
